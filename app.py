a909bb8234e88c96f896bc9e174964a4c3ea6a8e
Ian Porteous <ianp@checkpoint.com>
2019-01-07 11:52:23 +0000

    update to v.02

--- app.py
+++ app.py
@@ -5,16 +5,26 @@ debug = False
 testdomains = ['outlook.com','hotmail.com', '7e837e8378e328.com', 'ba.com', 'checkpoint.com', 'google.com', 'aaa.com']
 
 def CheckDomains(domains):
-    o365domains = []
+    results = {}
     for domain in domains:
         try:
             for mx in dns.resolver.query(domain, 'MX'):
                 if "protection.outlook.com" in mx.to_text():
-                    o365domains.append(domain)
+                    results[domain] = "O365"
+                elif "aspmx.l.google.com" in mx.to_text():
+                    results[domain] = "GSuite"
+                else:
+                    results[domain] = "Someting else"
         except:
             if debug:
                 print("Error:\t{} isn't a valid domain".format(domain))
-    return o365domains
+    return results
+
+def Results(r):
+    print(r)
+
+#res = CheckDomains(testdomains)
+#Results(res)
 
 app = Flask(__name__)
 
@@ -26,7 +36,7 @@ def form():
 def json_example():
     form_domains = request.form['domains'].split("\r\n")
     out = CheckDomains(form_domains)
-    return render_template('result.html', result = out)
+    return render_template('result.1.html', result = out)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True, use_reloader=True)
